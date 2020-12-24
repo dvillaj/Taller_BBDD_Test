99a69c36f99579c3eda9f9bbbfb85b7c8781c979
Daniel Villanueva Jiménez <dvillaj@gmail.com>
2020-12-24 05:30:01 +0000

    Postgres tested

--- functions.sh
+++ functions.sh
@@ -1,4 +1,3 @@
-
 function join_by { local d=$1; shift; local f=$1; shift; printf %s "$f" "${@/#/$d}"; }
 
 function execute_notebook {
@@ -7,10 +6,25 @@ function execute_notebook {
     LEN=`expr ${#ARRAY[@]} - 1`
     
     HOME_OUTPUT=`join_by / "${ARRAY[@]:0:$LEN}"`
-    HOME_OUTPUT="${HOME_OUTPUT:1}"
+    HOME_OUTPUT="./html/${HOME_OUTPUT:1}"
     
     jupyter nbconvert --ExecutePreprocessor.timeout=-1 --execute --output-dir=$HOME_OUTPUT --to HTML "$NOTEBOOK"
     if [ $? -eq 0 ]; then
         jupyter nbconvert --clear-output --inplace "$NOTEBOOK"
     fi
 }
+
+: '
+NOTEBOOK="../Taller_BBDD_Practica/dff/Caso de Uso.ipynb"
+IFS='/' read -r -a ARRAY <<< "$NOTEBOOK"
+LEN=`expr ${#ARRAY[@]} - 1`
+    
+echo "${ARRAY[@]}"
+    
+DIRECTORY=`join_by / "${ARRAY[@]:0:$LEN}"`
+HOME_OUTPUT="${DIRECTORY:1}"
+
+echo $DIRECTORY
+echo $HOME_OUTPUT
+'
+
